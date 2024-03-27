843f4aa42c2a7c5be9b83b504212a9286cc84612
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2024-03-27 12:51:05 +0530

    cors-enabled

--- app.js
+++ app.js
@@ -9,29 +9,7 @@ const connection = mysql.createConnection({
     password: '', // Change this to your MySQL password
     database: 'rrbeatlemeco_db' // Change this to your MySQL database name
 });
-// connection.connect((err) => {
-//   if (err) {
-//     console.error('Error connecting to database:', err);
-//     return;
-//   }
-//   console.log('Connected to database.');
-
-//   // Query to get all tables
-//   const query = "SELECT * FROM videos";
-//   connection.query(query, [connection.config.database], (error, results, fields) => {
-//     if (error) {
-//       console.error('Error executing query:', error);
-//       return;
-//     }
-//     console.log('List of tables:');
-//     results.forEach((row) => {
-//       console.log(row.table_name);
-//     });
-
-//     // Don't forget to close the connection when you're done
-//     connection.end();
-//   });
-// });
+const cors=require("cors");
 const videoRoutes=require('./Routes/videoRoutes.js');
 const audioBookRoutes=require("./Routes/audioBookRoutes.js")
 const categoryRoutes=require("./Routes/categoryRoutes.js");
@@ -48,6 +26,7 @@ const creatorRoutes=require("./Routes/creatorRoutes.js");
 const otherRoutes=require("./Routes/otherRoutes.js");
 const port = process.env.PORT || 8082;
 app.use(express.json());
+app.use(cors());
 app.use("/videos",videoRoutes);
 app.use("/audiobooks",audioBookRoutes);
 app.use("/category",categoryRoutes);
