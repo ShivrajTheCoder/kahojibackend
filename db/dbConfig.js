6280cac77e6140a7ade8a96f37b2680c041609ff
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2024-04-04 10:07:30 +0530

    start-commnad-added

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,8 +1,17 @@
+//hosted
 const dbConfig = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'rrbeatlemeco_db',
+    host: '97.74.91.11',
+    user: 'rrbeatlemeco_kahog',
+    password: 'kahog@123',
+    database: 'rrbeatlemeco_testkahog',
   };
+
+  // local
+  // const dbConfig = {
+  //   host: '97.74.91.11',
+  //   user: 'rrbeatlemeco_kahog',
+  //   password: 'kahog@123',
+  //   database: 'rrbeatlemeco_testkahog',
+  // };
 
   module.exports=dbConfig
