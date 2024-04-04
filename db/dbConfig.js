3a48e4d2e283eebc67787f09464ddf16194e36ba
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2024-04-04 14:04:14 +0530

    porduction-db-used

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,17 +1,17 @@
 //hosted
-// const dbConfig = {
-//     host: '97.74.91.11',
-//     user: 'rrbeatlemeco_kahog',
-//     password: 'kahog@123',
-//     database: 'rrbeatlemeco_testkahog',
-//   };
+const dbConfig = {
+    host: '97.74.91.11',
+    user: 'rrbeatlemeco_kahog',
+    password: 'kahog@123',
+    database: 'rrbeatlemeco_testkahog',
+  };
 
 // local
-const dbConfig = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'rrbeatlemeco_db',
-};
+// const dbConfig = {
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'rrbeatlemeco_db',
+// };
 
 module.exports = dbConfig
